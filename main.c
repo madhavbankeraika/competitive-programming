194fd02a52a6fe08e9b77290024eec5b1183b55e
madhavbankeraika <132508784+madhavbankeraika@users.noreply.github.com>
2023-06-13 18:45:19 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int q;
+    scanf("%d", &q);
+    while (q--) {
+        int n, t;
+        scanf("%d%d", &n, &t);
+        int a[n], b[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &b[i]);
+        }
+        int max_ent = -1, max_index = -1;
+        for (int i = 0; i < n; i++) {
+            if (a[i] <= t && b[i] > max_ent) {
+                max_ent = b[i];
+                max_index = i;
+            }
+        }
+        printf("%d\n", max_index + 1);
+    }
+    return 0;
+}
