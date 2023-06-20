91e4fb1ed8b8977591d919f7a39a09025825946a
madhavbankeraika <132508784+madhavbankeraika@users.noreply.github.com>
2023-06-20 15:49:56 +0530

    Add files via upload

--- main (5).c
+++ main (5).c
@@ -1,22 +1,19 @@
 #include<stdio.h>
-
+#include<math.h>
+int dividetwo(int divi,int divis)
+{
+   int quotient=(divi/divis);
+  quotient=floor(quotient);
+  printf("%d",(quotient));
+  }
 int main()
 {
-    int B;
-    printf("enter number of bananas: ");
-    scanf("%d",&B);
-    int k;
-    printf("cost of one banana: ");
-    scanf("%d",&k);
-    int cost=0;
-    int n;
-    printf("money the soldier has: ");
-    scanf("%d",&n);
-    for(int i=1;i<=B;i++)
-    {
-        cost=cost+i*k;
-    }
-    printf("cost: %d\n",cost);
-    printf("money borrowed from friend: %d",cost-n);
-    return 0;
+int dividend;
+  int divisor;
+  printf("enter the dividend\n");
+  scanf("%d",&dividend);
+  printf("enter the divisor\n");
+  scanf("%d",&divisor);
+  dividetwo(dividend,divisor);
+
 }
