f10366c880645657423e66bde5244a62d5fabc4d
madhavbankeraika <132508784+madhavbankeraika@users.noreply.github.com>
2023-06-20 15:49:41 +0530

    Add files via upload

--- main (6).c
+++ main (6).c
@@ -1,34 +1,42 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    int t;
-    scanf("%d", &t);
-
-    while (t--) {
-        char s[105];
-        scanf("%s", s);
-
-        int n = strlen(s);
-        int cnt = 0, prev = -2;
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '^') {
-                if (i - prev == 2) {
-                    cnt++;
-                    prev = i;
-                } else if (i - prev == 1) {
-                    prev = i;
-                }
-            }
-        }
-
-        printf("%d\n", cnt);
+#include<stdio.h>
+int main()
+{
+    int arr[5];
+   int  maxsum=0;
+   int minsum=0;
+  int max=arr[0];
+   int min=arr[0];
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d",&arr[i]);
     }
-
-    return 0;
+    for(int i=0;i<5;i++)
+    {
+      
+      if(arr[i]>arr[i+1])
+        {
+            max=arr[i];
+        }
+        
+         }
+         for(int i=0;i<5;i++)
+    { 
+        if(arr[i]<arr[i+1])
+        {
+            min=arr[i];
+        }
+        
 }
+  int sum=0;
+  for(int i=0;i<5;i++)
+    {
+      sum=sum+arr[i];
+    }
+maxsum=sum-max;
+  minsum=sum-min;
+  printf("%d %d",minsum,maxsum);
+  
+return 0;
 
-
-
+}
 
