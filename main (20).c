051b089368fdc07339a69981b14394bdc91ef6ef
madhavbankeraika <132508784+madhavbankeraika@users.noreply.github.com>
2023-07-03 22:26:34 +0530

    Add files via upload

--- main (20).c
+++ main (20).c
@@ -1,32 +1,42 @@
-#include <stdio.h>
-#include <limits.h>
-
-int main() {
-    int t;
-    scanf("%d", &t);
-
-    while (t--) {
-        int n;
-        scanf("%d", &n);
-
-        long long int max1 = LLONG_MIN, max2 = LLONG_MIN;
-        long long int num;
-
-        for (int i = 0; i < n; i++) {
-            scanf("%lld", &num);
-            
-            if (num > max1) {
-                max2 = max1;
-                max1 = num;
-            } else if (num > max2) {
-                max2 = num;
-            }
-        }
-
-        long long int beauty = max1 * max2;
-        printf("%lld\n", beauty);
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d\n",&n);
+  int arr[n];
+  for(int i=0;i<n;i++)
+    {
+      scanf("%d\n",&arr[i]);
+      
+        
     }
-
-    return 0;
+  int sumb=0;
+  int sumg=0;
+  for(int i=0;i<n;i++)
+    {
+      if(i%2==0)
+      {
+        sumb=sumb+arr[i];
+      }
+      if(i%2==1)
+      {
+        sumg=sumg+arr[i];
+      }
+    }
+  
+  int cho;
+  scanf("%c", &cho);
+  if(cho=='b')
+  {
+    printf("%d", sumb);
+    
+  }
+  if(cho=='g')
+  {
+    printf("%d", sumg);
+    
+  }
+  return 0;
+  
+  
 }
-
