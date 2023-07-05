8361eb236c6b279fa7ea0dba738d4c72d806f6a9
madhavbankeraika <132508784+madhavbankeraika@users.noreply.github.com>
2023-07-05 20:12:04 +0530

    Add files via upload

--- main (21).c
+++ main (21).c
@@ -1,47 +1,34 @@
+#include <math.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 
-bool isPalindrome(char* str, int start, int end) {
-    while (start < end) {
-        if (str[start] != str[end])
-            return false;
-        start++;
-        end--;
+int main(){
+    int n; 
+    scanf("%d",&n);
+    int *squares = malloc(sizeof(int) * n);
+    for(int squares_i = 0; squares_i < n; squares_i++){
+       scanf("%d",&squares[squares_i]);
     }
-    return true;
-}
-
-int longestNonPalindromeSubsequence(char* str) {
-    int len = strlen(str);
-    int start = 0;
-    int end = len - 1;
-
-    while (start <= end) {
-        if (str[start] != str[end])
-            return len;
-        else {
-            if (isPalindrome(str, start + 1, end - 1))
-                return -1;
-            start++;
-            end--;
+    int d; 
+    int m; 
+    scanf("%d %d",&d,&m);
+    // your code goes here
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        if (i + m <= n) {
+            for (int j = 0; j < m; j++) {
+                sum+=squares[i+j];
+            }
+            if (sum == d) {
+                count++;
+            }
         }
     }
-
-    return len - 1;
-}
-
-int main() {
-    int t;
-    scanf("%d", &t);
-
-    while (t--) {
-        char str[51];
-        scanf("%s", str);
-
-        int result = longestNonPalindromeSubsequence(str);
-        printf("%d\n", result);
-    }
-
+    printf("%d\n", count);
     return 0;
 }
